f7b79024112e11bcd561411123d68bced86e8821
Towfik <tufikhasan05@gmail>
2022-05-13 00:30:19 +0600

    try catch function added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,10 @@
 import postModel from '../models/postModel.js';
 
 export const getPosts = async (req, res) => {
-  const posts = await postModel.find();
-  res.status(200).json(posts);
+  try {
+    const posts = await postModel.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };

97c24a41a750455fae86eb438861343a83c20bde
Towfik <tufikhasan05@gmail>
2022-05-13 15:57:27 +0600

    Navbar logo setup for large and small device

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { makeStyles, AppBar, Toolbar, Typography } from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  logoLg: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'block',
+    },
+  },
+  logoSm: {
+    display: 'none',
+    [theme.breakpoints.down('md')]: {
+      display: 'block',
+    },
+  },
+}));
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const classes = useStyles();
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h6" className={classes.logoLg}>
+          SocialBook
+        </Typography>
+        <Typography variant="h6" className={classes.logoSm}>
+          SBook
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;

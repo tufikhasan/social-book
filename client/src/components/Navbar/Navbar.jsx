6818b4322c0cac953583c392c2040cf9a053b2b9
Towfik <tufikhasan05@gmail>
2022-05-13 21:16:01 +0600

    navbar search box, badge and avatar add

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import useStyles from './style';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  InputBase,
+  Avatar,
+} from '@material-ui/core';
+import Badge from '@material-ui/core/Badge';
+import SearchIcon from '@material-ui/icons/Search';
+import Mail from '@material-ui/icons/Mail';
+import Notifications from '@material-ui/icons/Notifications';
 
 const Navbar = () => {
   const classes = useStyles();
   return (
     <AppBar>
-      <Toolbar>
+      <Toolbar className={classes.toolbar}>
         <Typography variant="h6" className={classes.logoLg}>
           SocialBook
         </Typography>
         <Typography variant="h6" className={classes.logoSm}>
           SBook
         </Typography>
+        <div className={classes.search}>
+          <SearchIcon />
+          <InputBase placeholder="Search…" className={classes.input} />
+        </div>
+        <div className={classes.icons}>
+          <Badge badgeContent={4} color="secondary" className={classes.badge}>
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="secondary" className={classes.badge}>
+            <Notifications />
+          </Badge>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://previews.123rf.com/images/yupiramos/yupiramos1712/yupiramos171217688/92110199-elegant-businessman-avatar-character-vector-illustration-design.jpg"
+          />
+        </div>
       </Toolbar>
     </AppBar>
   );
--- client/src/components/Navbar/style.js
+++ client/src/components/Navbar/style.js
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, alpha } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   logoLg: {
@@ -12,6 +12,35 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up('sm')]: {
       display: 'none',
     },
+    marginRight: theme.spacing(1),
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  search: {
+    display: 'flex',
+    alignItems: 'center',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(1),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+    paddingLeft: theme.spacing(1),
+  },
+  input: {
+    color: 'white',
+  },
+  icons: {
+    display: 'flex',
+    alignItems: 'center',
   },
 }));
 

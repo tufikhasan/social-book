1cfc389a19985a1b6d0bdd98f1b72a96d83a8566
Towfik <tufikhasan05@gmail>
2022-05-13 23:06:14 +0600

    container margin added

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { Feed, Navbar, RightSidebar, SideBar } from './components';
-import { Box, Grid } from '@material-ui/core';
+import { makeStyles, Box, Grid } from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: theme.spacing(10),
+  },
+}));
 const App = () => {
+  const classes = useStyles();
   return (
     <Box>
       <Navbar />
-      <Grid container>
-        <Grid item sm={3}>
-          <SideBar />
+      <div className={classes.container}>
+        <Grid container>
+          <Grid item sm={3}>
+            <SideBar />
+          </Grid>
+          <Grid item sm={6}>
+            <Feed />
+          </Grid>
+          <Grid item sm={3}>
+            <RightSidebar />
+          </Grid>
         </Grid>
-        <Grid item sm={6}>
-          <Feed />
-        </Grid>
-        <Grid item sm={3}>
-          <RightSidebar />
-        </Grid>
-      </Grid>
+      </div>
     </Box>
   );
 };
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
   const classes = useStyles({ open });
   return (
-    <AppBar position="static">
+    <AppBar position="fixed">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h6" className={classes.logoLg}>
           SocialBook
